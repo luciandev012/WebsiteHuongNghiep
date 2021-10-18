df2f73ea2b85c3c218b6f6ec2808bf3acd6679b4
thang1224 <thanguk04@gmail.com>
2021-10-18 17:24:38 +0700

    add create

--- WebsiteHuongNghiep/Areas/Admin/Controllers/HollandMPController.cs
+++ WebsiteHuongNghiep/Areas/Admin/Controllers/HollandMPController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebsiteHuongNghiep.Application.Services;
+using WebsiteHuongNghiep.Application.ViewModels;
+using WebsiteHuongNghiep.Data.Entities;
 
 namespace WebsiteHuongNghiep.Areas.Admin.Controllers
 {
@@ -11,14 +14,36 @@ namespace WebsiteHuongNghiep.Areas.Admin.Controllers
     public class HollandMPController : Controller
     {
         private readonly IManageHLMultipleChoiceServices _manageHLMultipleChoice;
-        public HollandMPController(IManageHLMultipleChoiceServices manageHLMultipleChoice)
+        private readonly IManageHLTableServices _manageHLTable;
+        public HollandMPController(IManageHLMultipleChoiceServices manageHLMultipleChoice, IManageHLTableServices manageHLTable)
         {
             _manageHLMultipleChoice = manageHLMultipleChoice;
+            _manageHLTable = manageHLTable;
         }
         public async Task<IActionResult> Index()
         {
             var hlMultiples = await _manageHLMultipleChoice.GetAll();
             return View(hlMultiples);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var hlTables = await _manageHLTable.GetAll();
+            ViewBag.Tables = hlTables.Select(x => new SelectListItem() {
+                Text = x.Name,
+                Value = x.HLTableId.ToString(),
+            });
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(HollandMultipleChoice request)
+        {
+            var result = await _manageHLMultipleChoice.Create(request);
+            if(result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(request);
         }
     }
 }
